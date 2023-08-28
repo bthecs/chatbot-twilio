7893e7100060a435866be05ddbc10a58cd031b16
unknown <globalsol2023@gmail.com>
2023-08-28 11:01:32 -0300

    add

--- informe/arbol.py
+++ informe/arbol.py
@@ -1,53 +1,87 @@
-from graphviz import Digraph
-from anytree import Node
+# from graphviz import Digraph
+# from anytree import Node
+# import streamlit as st
+# from PIL import Image
+# from informe.constantes import Constants
+# import os
+
+
+# script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# # Ruta completa al ejecutable dot
+# graphviz_executable = os.path.abspath(os.path.join(script_dir, "..", "Graphviz", "bin", "dot"))
+
+
+# def arbol():
+#     st.image("informe/imagenes/price.png", caption='Precio de 1000 aperturas y 1000 mensajes', use_column_width=True)
+#     st.image("informe/imagenes/mensaje.png", caption='Mensaje utilizado para campaña', use_column_width=True)
+
+
+#     tupla = Constants().main()
+#     print(tupla[0])
+#     # Crear los nodos del árbol
+#     root = Node("Arbol")
+#     mensajes_node = Node(f"Mensajes {tupla[0]}", parent=root)
+#     responde_node = Node(f"Responde {tupla[1]}", parent=mensajes_node)
+#     si_node = Node(f"Si, me encantaría {tupla[2]}", parent=responde_node)
+#     si_porcentaje_node = Node(f"Porcentaje de derivación {tupla[7]}", parent=si_node)
+#     no_porcentaje_node = Node(f"Porcentaje de no derivación {tupla[8]}", parent=si_node)
+#     no_en_otro_momento_node = Node(f"No en otro momento {tupla[5]}", parent=responde_node)
+#     no_responde_node = Node(f"No responde {tupla[6]}", parent=mensajes_node)
+
+
+#     # Crear un objeto Digraph
+#     dot = Digraph(format='png', graph_attr={'rankdir': 'TB', 'bgcolor': 'black'}, engine=graphviz_executable)
+#     dot.attr('node', style='filled', shape='box', color='#D9E6F5', fontname='Courier', fontsize='10')
+#     dot.attr('edge', color='#808080')
+
+
+#     # Agregar las relaciones entre los nodos
+#     for node in root.descendants:
+#         if node.parent:
+#             dot.edge(node.parent.name, node.name)
+
+#     # Guarda en un archivo temporal
+#     graph_filename = "temp_graph"
+#     image_path = os.path.abspath(os.path.join(script_dir, "..", "informe", "imagenes", graph_filename))
+#     dot.render(image_path, cleanup=True)
+#     # Crear la interfaz de streamlit
+#     st.title("Arbol de Mensajes")
+
+#     tree_image = Image.open("informe/imagenes/temp_graph.png")
+#     st.image(tree_image, caption="Árbol de Mensajes", use_column_width=True, output_format="PNG")
+
 import streamlit as st
+import networkx as nx
+import matplotlib.pyplot as plt
 from PIL import Image
 from informe.constantes import Constants
-import os
-
-
-script_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Ruta completa al ejecutable dot
-graphviz_executable = os.path.abspath(os.path.join(script_dir, "..", "Graphviz", "bin", "dot"))
-
 
 def arbol():
     st.image("informe/imagenes/price.png", caption='Precio de 1000 aperturas y 1000 mensajes', use_column_width=True)
     st.image("informe/imagenes/mensaje.png", caption='Mensaje utilizado para campaña', use_column_width=True)
 
-
     tupla = Constants().main()
-    print(tupla[0])
-    # Crear los nodos del árbol
-    root = Node("Arbol")
-    mensajes_node = Node(f"Mensajes {tupla[0]}", parent=root)
-    responde_node = Node(f"Responde {tupla[1]}", parent=mensajes_node)
-    si_node = Node(f"Si, me encantaría {tupla[2]}", parent=responde_node)
-    si_porcentaje_node = Node(f"Porcentaje de derivación {tupla[7]}", parent=si_node)
-    no_porcentaje_node = Node(f"Porcentaje de no derivación {tupla[8]}", parent=si_node)
-    no_en_otro_momento_node = Node(f"No en otro momento {tupla[5]}", parent=responde_node)
-    no_responde_node = Node(f"No responde {tupla[6]}", parent=mensajes_node)
 
+    # Crear un grafo dirigido
+    G = nx.DiGraph()
 
-    # Crear un objeto Digraph
-    dot = Digraph(format='png', graph_attr={'rankdir': 'TB', 'bgcolor': 'black'}, engine=graphviz_executable)
-    dot.attr('node', style='filled', shape='box', color='#D9E6F5', fontname='Courier', fontsize='10')
-    dot.attr('edge', color='#808080')
+    # Agregar nodos al grafo
+    G.add_node("Arbol")
+    G.add_node(f"Mensajes {tupla[0]}")
+    G.add_edge(f"Mensajes {tupla[0]}", f"Responde {tupla[1]}")
+    # Agregar más nodos y bordes según tu lógica
 
-
-    # Agregar las relaciones entre los nodos
-    for node in root.descendants:
-        if node.parent:
-            dot.edge(node.parent.name, node.name)
-
-    # Guarda en un archivo temporal
-    graph_filename = "temp_graph"
-    image_path = os.path.abspath(os.path.join(script_dir, "..", "informe", "imagenes", graph_filename))
-    dot.render(image_path, cleanup=True)
     # Crear la interfaz de streamlit
     st.title("Arbol de Mensajes")
 
-    tree_image = Image.open("informe/imagenes/temp_graph.png")
-    st.image(tree_image, caption="Árbol de Mensajes", use_column_width=True, output_format="PNG")
+    # Dibujar el grafo y mostrarlo en Streamlit
+    pos = nx.spring_layout(G)  # Elegir una disposición de nodos
+    nx.draw(G, pos, with_labels=True, node_size=3000, node_color="skyblue", font_size=8, font_color="black", arrows=True)
+    plt.axis("off")
+    plt.tight_layout()
 
+    image_path = "informe/imagenes/temp_graph.png"
+    plt.savefig(image_path, format="png")
+    tree_image = Image.open(image_path)
+    st.image(tree_image, caption="Árbol de Mensajes", use_column_width=True, output_format="PNG")
