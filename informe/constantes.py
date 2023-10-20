8a3389fcdcaf1331ab2ea617b13584431ae65603
GS2023 <globalsol2023@gmail.com>
2023-10-20 12:38:30 -0300

    add

--- informe/constantes.py
+++ informe/constantes.py
@@ -1,35 +1,34 @@
 from database_module import get_number_of_records, get_number_of_records_by_filter
 
 
-class Constants():
-    def __init__(self):
-        self.cantidad_mensajes = None
-        self.responde = None
-        self.si = None
-        self.por_si = None
-        self.por_no = None
-        self.no_en_otro_momento = None
-        self.no = None
-        self.porcentaje_si = None
-        self.porcentaje_no = None
+
+cantidad_mensajes = None
+responde = None
+si = None
+por_si = None
+por_no = None
+no_en_otro_momento = None
+no = None
+porcentaje_si = None
+porcentaje_no = None
 
 
-    def main(self, total_mensajes):
-        print(total_mensajes)
-        total = len(total_mensajes)
-        self.cantidad_mensajes = total
-        responde_no = 1 # no responde
-        responde_si = 1 # si responde
-        responde = responde_si + responde_no
-        self.responde = responde
-        self.si = responde_si
-        self.por_si = self.si/self.responde
-        self.por_no = 1-self.por_si
-        self.no_en_otro_momento = self.responde-self.si
-        self.no = self.cantidad_mensajes-self.responde
-        self.porcentaje_si = "{:.2f}".format(self.por_si)
-        self.porcentaje_no = "{:.2f}".format(self.por_no)
-        return self.cantidad_mensajes, self.responde, self.si, self.por_si, self.por_no, self.no_en_otro_momento, self.no, self.porcentaje_si, self.porcentaje_no
+def main( total_mensajes):
+    print(total_mensajes)
+    total = len(total_mensajes)
+    cantidad_mensajes = total
+    responde_no = 1 # no responde
+    responde_si = 1 # si responde
+    responde = responde_si + responde_no
+    responde = responde
+    si = responde_si
+    por_si = si/responde
+    por_no = 1-por_si
+    no_en_otro_momento = responde-si
+    no = cantidad_mensajes-responde
+    porcentaje_si = "{:.2f}".format(por_si)
+    porcentaje_no = "{:.2f}".format(por_no)
+    return cantidad_mensajes, responde, si, por_si, por_no, no_en_otro_momento, no, porcentaje_si, porcentaje_no
 
 
 
