640993855c5059c116dc4da8e1b7d389c1efe66d
GS2023 <globalsol2023@gmail.com>
2023-10-20 12:35:48 -0300

    add

--- front.py
+++ front.py
@@ -8,7 +8,8 @@ import requests
 def get_all_data():
     url = "https://12ee-207-248-125-20.ngrok-free.app/get-all-data"
     response = requests.get(url)
-    return Constants().main(response.json)
+    constantes = Constants()
+    return constantes.main(response.json)
 
 if __name__ == "__main__":
     scheduler = BackgroundScheduler()
