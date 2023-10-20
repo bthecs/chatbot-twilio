49dbdd67c7146b8a91b7989ec8f142fa4ebfc25c
GS2023 <globalsol2023@gmail.com>
2023-10-20 12:39:02 -0300

    add

--- front.py
+++ front.py
@@ -1,15 +1,14 @@
 from informe.arbol import arbol
 from informe.grafico import grafico
 from informe.mapa import mapa
-from informe.constantes import Constants
+from informe.constantes import main
 from apscheduler.schedulers.background import BackgroundScheduler
 import requests
 
 def get_all_data():
     url = "https://12ee-207-248-125-20.ngrok-free.app/get-all-data"
     response = requests.get(url)
-    constantes = Constants()
-    return constantes.main(response.json)
+    return main(response.json)
 
 if __name__ == "__main__":
     scheduler = BackgroundScheduler()
