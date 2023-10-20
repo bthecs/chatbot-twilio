d26be4be1eba05c7f94456c82a27c440750570e6
GS2023 <globalsol2023@gmail.com>
2023-10-20 15:09:42 -0300

    final

--- front.py
+++ front.py
@@ -23,21 +23,9 @@ def get_all_data():
     return main(total_mensajes.json(), si_resp.json(), no_resp.json())
 
 if __name__ == "__main__":
-    url = "https://12ee-207-248-125-20.ngrok-free.app/get-all-data"
-    total_mensajes = requests.get(url)
-    url_2 = "https://12ee-207-248-125-20.ngrok-free.app/get-filter-data"
-    json_data = {
-        "value": "si, me encantaria",
-        "column": "initial_message"
-    }
-    si_resp = requests.get(url_2, json=json_data)
-    json_data_2 = {
-        "value": "en otro momento",
-        "column": "initial_message"
-    }
-    no_resp = requests.get(url_2, json=json_data_2)
-    
-    main(total_mensajes.json(), si_resp.json(), no_resp.json())
+    mapa(0)
+    arbol(0, 0, 0, 0, 0, 0, 0, 0)
+    grafico(0, 0, 0)
     scheduler = BackgroundScheduler()
     scheduler.add_job(get_all_data, 'interval', seconds=10)
     scheduler.start()
