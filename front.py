4b6e7926cb15368450615c40e21ae02e5226e8d0
bthecs <lautarogimenez897@gmail.com>
2023-11-02 10:08:57 -0300

    add

--- front.py
+++ front.py
@@ -9,9 +9,9 @@ def get_all_data():
     url = "https://ccdd-207-248-125-79.ngrok-free.app/get-all-data"
     total_mensajes = requests.get(url)
     url_2 = "https://ccdd-207-248-125-79.ngrok-free.app/get-filter-data"
-    json_data = {"value": "si, me encantaria","column": "initial_message"}
+    json_data = {"value": "contactar con asesor","column": "initial_message"}
     si_resp = requests.get(url_2, json=json_data)
-    json_data_2 = {"value": "en otro momento","column": "initial_message"}
+    json_data_2 = {"value": "no, gracias","column": "initial_message"}
     no_resp = requests.get(url_2, json=json_data_2)
     
     return main(total_mensajes.json(), si_resp.json(), no_resp.json())
