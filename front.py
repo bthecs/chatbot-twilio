388550b2516c75ab4a42304d3905be6599196e73
bthecs <lautarogimenez897@gmail.com>
2023-11-13 09:26:48 -0300

    add

--- front.py
+++ front.py
@@ -6,9 +6,9 @@ def get_all_data():
     url = "https://ea1b-207-248-125-79.ngrok-free.app/get-all-data"
     total_mensajes = requests.get(url)
     url_2 = "https://ea1b-207-248-125-79.ngrok-free.app/get-filter-data"
-    json_data = {"value": "contactar con asesor","column": "initial_message"}
+    json_data = {"value": "contactar con asesor","column": "usage_interest"}
     si_resp = requests.get(url_2, json=json_data)
-    json_data_2 = {"value": "no, gracias","column": "usage_interest"}
+    json_data_2 = {"value": "no, gracias","column": "initial_message"}
     no_resp = requests.get(url_2, json=json_data_2)
     url_3 = "https://ea1b-207-248-125-79.ngrok-free.app/get-column-data"
     json_column = {"column": "initial_message"}
