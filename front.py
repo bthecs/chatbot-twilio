52ad6c2b1d82d177b4a6e316ac4bdd5d947760bc
bthecs <lautarogimenez897@gmail.com>
2023-11-15 12:23:17 -0300

    add

--- front.py
+++ front.py
@@ -3,14 +3,14 @@ from informe.constantes import main
 import requests
 
 def get_all_data():
-    url = "https://ea1b-207-248-125-79.ngrok-free.app/get-all-data"
+    url = "https://08d4-207-248-125-79.ngrok-free.app/get-all-data"
     total_mensajes = requests.get(url)
-    url_2 = "https://ea1b-207-248-125-79.ngrok-free.app/get-filter-data"
+    url_2 = "https://08d4-207-248-125-79.ngrok-free.app/get-filter-data"
     json_data = {"value": "contactar con asesor","column": "usage_interest"}
     si_resp = requests.get(url_2, json=json_data)
     json_data_2 = {"value": "no, gracias","column": "initial_message"}
     no_resp = requests.get(url_2, json=json_data_2)
-    url_3 = "https://ea1b-207-248-125-79.ngrok-free.app/get-column-data"
+    url_3 = "https://08d4-207-248-125-79.ngrok-free.app/get-column-data"
     json_column = {"column": "initial_message"}
     message_column = requests.get(url_3, json=json_column)
     
